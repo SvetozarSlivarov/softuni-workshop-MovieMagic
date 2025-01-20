5b177a7754972c4a4459a1f3915530162143418f
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-01-20 18:39:52 +0200

    Install Handlebars

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import handlebars from 'express-handlebars'
+
+const app = express();
+const port = 5000;
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: false})
+});
+
+app.listen(port, () => console.log('Server is listening on http://localhost:5000...'));
