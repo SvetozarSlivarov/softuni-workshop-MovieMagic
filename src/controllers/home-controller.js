ec5c0d5cae79416a40688e54d373e6d01f8985d4
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-01-21 23:51:52 +0200

    Add getAll function

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,11 +1,12 @@
 import {Router} from 'express';
+import movieService from '../services/movie-service.js';
 
-import movies from '../movies.js';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    console.log(movies);
+    const movies = movieService.getAll();
+    
     res.render('home', {movies});
 });
 
