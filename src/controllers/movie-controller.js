4f842b5e12b520af5c5ea1873baa0f4702eff25b
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-01-21 22:22:36 +0200

    Add movie service

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,9 +1,19 @@
 import { Router  } from "express";
+import movieService from "../services/movie-service.js";
 
 const movieController = Router();
 
 movieController.get('/create', (req, res) => {
     res.render('create');
 });
+movieController.get('/:movieId/details', (req, res) => {
+    const movieId = req.params.movieId;
+
+    console.log(movieId);
+    const movie = movieService.findMovie(movieId);
+    console.log(movie);
+
+    res.render('details');
+})
 
 export default movieController;
---
+++ src/services/movie-service.js
@@ -0,0 +1,8 @@
+import movies from '../movies.js'
+
+export default {
+    findMovie(movieId){
+        const result = movies.find(movie => movie.id === movieId);
+        return result;
+    }
+}
