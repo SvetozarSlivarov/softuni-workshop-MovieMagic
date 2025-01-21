5b9601295fa7af6fc8c9a42186a6e2ec72d54153
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-01-21 23:01:03 +0200

    Parse form data on server

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -6,6 +6,14 @@ const movieController = Router();
 movieController.get('/create', (req, res) => {
     res.render('create');
 });
+movieController.post('/create', (req, res) => {
+    console.log('POST MOVIE');
+
+    console.log(req.body);
+
+    res.end();
+
+})
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use('/static',express.static('src/public'))
+app.use('/static',express.static('src/public'));
+app.use(express.urlencoded({extended: false}));
 
 app.use(routes);
 
