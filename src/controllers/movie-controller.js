bfd987921c3e9d756f168464f1b0ea3c4dce6c54
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-02-01 02:26:20 +0200

    Update movie-controller.js

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -4,7 +4,6 @@ import movieService from "../services/movie-service.js";
 const movieController = Router();
 
 movieController.get('/search', (req, res) => {
-    console.log(req.query)
     const filter = req.query
     const movies = movieService.getAll(filter);
     res.render('search', {movies, filter});
