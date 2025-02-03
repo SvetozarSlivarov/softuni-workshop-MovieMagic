683414426c72cc39a75bef312e1b3dbab8ab2ee9
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-02-04 00:36:35 +0200

    Config handlebars to use mongoose documents

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ try{
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true
+    },
     helpers: {
         showRating: showRatingHelper,
     }
