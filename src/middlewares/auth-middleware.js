695dee3fd040a6c1abe88caf9d58abe4fc46548a
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-02-06 23:34:58 +0200

    Clear cookie for invalid auth tokens

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -19,6 +19,8 @@ export const authMiddleware = (req, res, next) => {
         next();
     } catch(err) {
         //TODO: Invalid token
+        res.clearCookie('auth');
+        res.redirect('/auth/login');
     }
     
     
