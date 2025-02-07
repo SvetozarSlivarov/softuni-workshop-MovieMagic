4dd89148cab71f314cac94cedb82255f1e6041ed
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-02-07 02:25:27 +0200

    Update movie from edit

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -84,4 +84,13 @@ movieController.get('/:movieId/edit',async (req, res) => {
     res.render('movie/edit', { movie, categories });
 });
 
+movieController.post('/:movieId/edit', async (req, res) => {
+    const movieData = req.body;
+    const movieId = req.params.movieId;
+
+    await movieService.update(movieId, movieData);
+
+    res.redirect('/');
+});
+
 export default movieController;
